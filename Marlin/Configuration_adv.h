7e9cb7c1306066ff89b08905a30ef9cb413c003e
zbakhaya <zbakhaya@users.noreply.github.com>
2020-10-30 13:59:41 -0500

    fix probe_offset_start error
    value must be < 0; Set to -2.0

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1081,7 +1081,7 @@
   #if HAS_BED_PROBE
     #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START 0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_START -2.0   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
   #endif
 
